373363b64cefc8c2c6d8a33dccf6e1b823bb458c
Jinhyunly <dbdbddipp@gmail.com>
2022-02-11 22:09:23 +0900

    study

---
+++ baeckjoon/sort/1181/1181.py
@@ -0,0 +1,15 @@
+import sys
+
+n = int(sys.stdin.readline())
+
+arr = []
+
+for i in range(n):
+  arr.append(sys.stdin.readline().strip())
+
+arr = list(set(arr))
+arr.sort()
+arr.sort(key=lambda x : len(x))
+
+for i in arr:
+  print(i)
---
+++ baeckjoon/sort/1427/1427.py
@@ -0,0 +1,11 @@
+import sys
+
+n = int(sys.stdin.readline())
+
+arr = []
+for i in str(n):
+  arr.append(int(i))
+
+arr.sort(reverse = True)
+for i in arr:
+  print(i, end = '')
