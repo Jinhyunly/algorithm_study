6fa9df7f8264bf9aff401f58364b508b67170e5a
Jinhyunly <dbdbddipp@gmail.com>
2022-02-21 21:21:24 +0900

    10872

--- baeckjoon/other/10872/10872.py
+++ baeckjoon/other/10872/10872.py
@@ -3,3 +3,18 @@ import sys
 
 n = int(sys.stdin.readline())
 print(math.factorial(n))
+
+
+--------------------------------
+
+import sys
+
+n = int(sys.stdin.readline())
+
+def fac(n):
+  result = 1
+  for i in range(1, n+1):
+    result = result * i
+  return result
+  
+print(fac(n))
