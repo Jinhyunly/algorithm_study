4ac5bb9b711b0876629fa0cfe83d7d5fee973bd2
Jinhyunly <dbdbddipp@gmail.com>
2022-03-08 21:38:36 +0900

    1427 modify

--- baeckjoon/sort/1427/1427.py
+++ baeckjoon/sort/1427/1427.py
@@ -9,3 +9,15 @@ for i in str(n):
 arr.sort(reverse = True)
 for i in arr:
   print(i, end = '')
+
+
+#################################
+# 리펙토링
+
+nums = input()
+nums = [int(n) for n in nums]
+
+ordered_nums = sorted(nums, reverse = True)
+
+for n in ordered_nums:
+  print(n, end='')
